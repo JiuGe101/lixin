3ccf25cc4b9389c9cc86a8637f3a3bd735b4d4a4
JiuGe101 <780338669@qq.com>
2024-10-14 23:07:11 +0800

    14_log_output_ope

---
+++ 04_Software/14_log_output_ope/MDK-ARM/RTE/_14_log_output_ope/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: '14_log_output_ope' 
+ * Target:  '14_log_output_ope' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
