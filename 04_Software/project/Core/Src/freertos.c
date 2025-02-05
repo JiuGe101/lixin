a484d8eb34f697e611e275e15650a11720646196
JiuGe101 <780338669@qq.com>
2025-02-05 18:21:59 +0800

    offline_breakpoint#3

--- 04_Software/project/Core/Src/freertos.c
+++ 04_Software/project/Core/Src/freertos.c
@@ -26,6 +26,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "elog.h"
+#include "stdio.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -45,14 +46,14 @@
 
 /* Private variables ---------------------------------------------------------*/
 /* USER CODE BEGIN Variables */
-
+extern volatile uint32_t debug_var;
 /* USER CODE END Variables */
 /* Definitions for defaultTask */
 osThreadId_t defaultTaskHandle;
 const osThreadAttr_t defaultTask_attributes = {
-    .name = "defaultTask",
-    .stack_size = 128 * 4,
-    .priority = (osPriority_t) osPriorityNormal,
+  .name = "defaultTask",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
 };
 
 /* Private function prototypes -----------------------------------------------*/
@@ -70,37 +71,37 @@ void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
   * @retval None
   */
 void MX_FREERTOS_Init(void) {
-    /* USER CODE BEGIN Init */
+  /* USER CODE BEGIN Init */
 
-    /* USER CODE END Init */
+  /* USER CODE END Init */
 
-    /* USER CODE BEGIN RTOS_MUTEX */
+  /* USER CODE BEGIN RTOS_MUTEX */
     /* add mutexes, ... */
-    /* USER CODE END RTOS_MUTEX */
+  /* USER CODE END RTOS_MUTEX */
 
-    /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
     /* add semaphores, ... */
-    /* USER CODE END RTOS_SEMAPHORES */
+  /* USER CODE END RTOS_SEMAPHORES */
 
-    /* USER CODE BEGIN RTOS_TIMERS */
+  /* USER CODE BEGIN RTOS_TIMERS */
     /* start timers, add new ones, ... */
-    /* USER CODE END RTOS_TIMERS */
+  /* USER CODE END RTOS_TIMERS */
 
-    /* USER CODE BEGIN RTOS_QUEUES */
+  /* USER CODE BEGIN RTOS_QUEUES */
     /* add queues, ... */
-    /* USER CODE END RTOS_QUEUES */
+  /* USER CODE END RTOS_QUEUES */
 
-    /* Create the thread(s) */
-    /* creation of defaultTask */
-    defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+  /* Create the thread(s) */
+  /* creation of defaultTask */
+  defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
 
-    /* USER CODE BEGIN RTOS_THREADS */
+  /* USER CODE BEGIN RTOS_THREADS */
     /* add threads, ... */
-    /* USER CODE END RTOS_THREADS */
+  /* USER CODE END RTOS_THREADS */
 
-    /* USER CODE BEGIN RTOS_EVENTS */
+  /* USER CODE BEGIN RTOS_EVENTS */
     /* add events, ... */
-    /* USER CODE END RTOS_EVENTS */
+  /* USER CODE END RTOS_EVENTS */
 
 }
 
@@ -113,21 +114,25 @@ void MX_FREERTOS_Init(void) {
 /* USER CODE END Header_StartDefaultTask */
 void StartDefaultTask(void *argument)
 {
-    /* USER CODE BEGIN StartDefaultTask */
+  /* USER CODE BEGIN StartDefaultTask */
     /* Infinite loop */
-	log_i("Default Task");
+	printf("Default Task\r\n");
+	//log_i("Default Task");
+	printf("Default Task1\r\n");
 	int a = 10;
 	int b = 5;
 	int c = a / b;
+	debug_var = 1;
+	printf("Default Task2\r\n");
 	// __BKPT(1);
 	// *WATCH_ADDRESS = 0x5678;
     for(;;)
     {
-		__BKPT(1);
-		log_i("Hello");
+		// __BKPT(1);
+		//log_i("Hello");
         osDelay(100);
     }
-    /* USER CODE END StartDefaultTask */
+  /* USER CODE END StartDefaultTask */
 }
 
 /* Private application code --------------------------------------------------*/
--- 04_Software/project/Core/Src/main.c
+++ 04_Software/project/Core/Src/main.c
@@ -19,12 +19,14 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "cmsis_os.h"
+#include "usart.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "SEGGER_RTT.h"
 #include "elog.h"
+#include "stdio.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -45,66 +47,23 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-
+volatile uint32_t debug_var __attribute__((section(".ARM.__at_0x20000000")));
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 void MX_FREERTOS_Init(void);
-void MPU_Config(void)
-{
-    HAL_MPU_Disable();
-    // 关闭（禁用）MPU，这一步是在配置 MPU 前确保 MPU 是关闭状态。这样可以安全地进行配置。
-
-    // 配置MPU保护区域（将指定地址设为只读）
-    // 下面是 MPU 的配置步骤，我们将定义一个特定的内存区域，并设置它的访问权限。
-    
-    MPU_Region_InitTypeDef MPU_InitStruct;
-    // 创建一个 `MPU_Region_InitTypeDef` 结构体实例，用于存储 MPU 区域的配置参数。
-
-    MPU_InitStruct.Enable = MPU_REGION_ENABLE;
-    // 启用该 MPU 区域。此选项告诉 MPU 这个区域是有效的，并且将被保护。
-
-    MPU_InitStruct.BaseAddress = (uint32_t)WATCH_ADDRESS;
-    // 设置 MPU 保护区域的基地址（起始地址）。`WATCH_ADDRESS` 是你要监视和保护的内存地址。
-
-    MPU_InitStruct.Size = MPU_REGION_SIZE_32B;
-    // 设置内存区域的大小。此处设置为 32 字节大小，也就是该区域的总内存范围是 32 字节。
-
-    MPU_InitStruct.AccessPermission = MPU_REGION_NO_ACCESS;
-    // 设置访问权限。此处为 `MPU_REGION_NO_ACCESS`，表示该区域内没有访问权限，任何访问都会触发异常。
-
-    MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
-    // 设置是否允许缓冲。此处为 `MPU_ACCESS_NOT_BUFFERABLE`，表示该区域内的访问不允许被缓冲。
-
-    MPU_InitStruct.IsCacheable = MPU_ACCESS_CACHEABLE;
-    // 设置是否允许缓存。此处为 `MPU_ACCESS_CACHEABLE`，表示该区域的内存访问是可缓存的。（STM32F411CEU6只有在Flash上有Cache缓存机制，如果你的Watch点是在Flash上，那么这个选项又有，如果你的Watch点在RAM上，这个选项并不会增加访问性能）
-
-    MPU_InitStruct.IsShareable = MPU_ACCESS_NOT_SHAREABLE;
-    // 设置是否允许多个处理器共享该区域的访问。此处为 `MPU_ACCESS_NOT_SHAREABLE`，表示该区域不允许共享。（STM32F411CEU6 是基于单核的 Cortex-M4 的单核架构，因此配置中的 IsShareable 参数在单核处理器上并没有实际的作用。）
-
-    MPU_InitStruct.Number = MPU_REGION_NUMBER0;
-    // 设置 MPU 区域编号。此处为 `MPU_REGION_NUMBER0`，表示这是 MPU 的第一个区域。
-
-    MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
-    // 设置内存类型扩展字段，`TEX` 用于定义内存类型。此处设置为 `MPU_TEX_LEVEL0`，通常表示普通内存类型。
-
-    MPU_InitStruct.SubRegionDisable = 0x00;
-    // 设置子区域禁用位。`0x00` 表示不禁用任何子区域，整个区域都是有效的。
-
-    MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_DISABLE;
-    // 设置是否允许从该区域执行代码。此处为 `MPU_INSTRUCTION_ACCESS_DISABLE`，表示不允许执行任何指令，防止恶意代码执行。
-
-    HAL_MPU_ConfigRegion(&MPU_InitStruct);
-    // 使用 `HAL_MPU_ConfigRegion` 函数配置 MPU 区域。将上述设置应用到第一个 MPU 区域。
-
-    // 启用MPU
-    HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
-    // 重新启用 MPU，并设置默认的权限模式为 `MPU_PRIVILEGED_DEFAULT`。这意味着只有特权模式才能访问没有配置的区域。
-}
-
 /* USER CODE BEGIN PFP */
-
+void dwt_watchpoint_enable()
+{
+  CoreDebug->DEMCR = CoreDebug_DEMCR_TRCENA_Msk |  // enable tracing
+                     CoreDebug_DEMCR_MON_EN_Msk;  // enable debug interrupt
+  DWT->FUNCTION0 = 0;    // disable comparator
+  DWT->COMP0 = 0x20000000;  // the address to monitor
+  DWT->MASK0 = 0;      // match all comparator bits, don't ignore any
+  DWT->FUNCTION0 = (2UL << DWT_FUNCTION_DATAVSIZE_Pos) |  // compare data size is word
+           (6UL << DWT_FUNCTION_FUNCTION_Pos);    // generate a watchpoint on write 
+}
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -119,30 +78,31 @@ void MPU_Config(void)
 int main(void)
 {
 
-    /* USER CODE BEGIN 1 */
+  /* USER CODE BEGIN 1 */
 
-    /* USER CODE END 1 */
+  /* USER CODE END 1 */
 
-    /* MCU Configuration--------------------------------------------------------*/
+  /* MCU Configuration--------------------------------------------------------*/
 
-    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-    HAL_Init();
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
 
-    /* USER CODE BEGIN Init */
-	*WATCH_ADDRESS = 0x1234;
-	MPU_Config();
-    /* USER CODE END Init */
+  /* USER CODE BEGIN Init */
+	//*WATCH_ADDRESS = 0x1234;
+	// MPU_Config();
+  /* USER CODE END Init */
 
-    /* Configure the system clock */
-    SystemClock_Config();
+  /* Configure the system clock */
+  SystemClock_Config();
 
-    /* USER CODE BEGIN SysInit */
+  /* USER CODE BEGIN SysInit */
+	dwt_watchpoint_enable();
+  /* USER CODE END SysInit */
 
-    /* USER CODE END SysInit */
-
-    /* Initialize all configured peripherals */
-    MX_GPIO_Init();
-    /* USER CODE BEGIN 2 */
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART1_UART_Init();
+  /* USER CODE BEGIN 2 */
     SEGGER_RTT_Init();
     /* close printf buffer */
     //setbuf(stdout, NULL);
@@ -157,28 +117,35 @@ int main(void)
     elog_set_fmt(ELOG_LVL_VERBOSE, ELOG_FMT_ALL & ~ELOG_FMT_FUNC);
     /* start EasyLogger */
     elog_start();
-    /* USER CODE END 2 */
+  /* USER CODE END 2 */
 
-    /* Init scheduler */
-    osKernelInitialize();
+  /* Init scheduler */
+  osKernelInitialize();
 
-    /* Call init function for freertos objects (in cmsis_os2.c) */
-    MX_FREERTOS_Init();
+  /* Call init function for freertos objects (in cmsis_os2.c) */
+  MX_FREERTOS_Init();
 
-    /* Start scheduler */
-    osKernelStart();
+  /* Start scheduler */
+  osKernelStart();
 
-    /* We should never get here as control is now taken by the scheduler */
+  /* We should never get here as control is now taken by the scheduler */
 
-    /* Infinite loop */
-    /* USER CODE BEGIN WHILE */
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  dwt_watchpoint_enable();
+	printf("main\r\n");
+	debug_var = 2;
+	//debug_var = 5;
     while (1)
     {
-        /* USER CODE END WHILE */
+		//debug_var = 5;
+		printf("hello\r\n");
+		HAL_Delay(1000);
+    /* USER CODE END WHILE */
 
-        /* USER CODE BEGIN 3 */
+    /* USER CODE BEGIN 3 */
     }
-    /* USER CODE END 3 */
+  /* USER CODE END 3 */
 }
 
 /**
@@ -187,44 +154,44 @@ int main(void)
   */
 void SystemClock_Config(void)
 {
-    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-    /** Configure the main internal regulator output voltage
-    */
-    __HAL_RCC_PWR_CLK_ENABLE();
-    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-    /** Initializes the RCC Oscillators according to the specified parameters
-    * in the RCC_OscInitTypeDef structure.
-    */
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-    RCC_OscInitStruct.PLL.PLLM = 8;
-    RCC_OscInitStruct.PLL.PLLN = 100;
-    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-    RCC_OscInitStruct.PLL.PLLQ = 4;
-    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-    {
-        Error_Handler();
-    }
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 100;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
 
-    /** Initializes the CPU, AHB and APB buses clocks
-    */
-    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
-                                  | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
-    {
-        Error_Handler();
-    }
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
+  {
+    Error_Handler();
+  }
 }
 
 /* USER CODE BEGIN 4 */
@@ -241,15 +208,15 @@ void SystemClock_Config(void)
   */
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
-    /* USER CODE BEGIN Callback 0 */
+  /* USER CODE BEGIN Callback 0 */
 
-    /* USER CODE END Callback 0 */
-    if (htim->Instance == TIM11) {
-        HAL_IncTick();
-    }
-    /* USER CODE BEGIN Callback 1 */
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM11) {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
 
-    /* USER CODE END Callback 1 */
+  /* USER CODE END Callback 1 */
 }
 
 /**
@@ -258,13 +225,13 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
   */
 void Error_Handler(void)
 {
-    /* USER CODE BEGIN Error_Handler_Debug */
+  /* USER CODE BEGIN Error_Handler_Debug */
     /* User can add his own implementation to report the HAL error return state */
     __disable_irq();
     while (1)
     {
     }
-    /* USER CODE END Error_Handler_Debug */
+  /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
@@ -277,9 +244,9 @@ void Error_Handler(void)
   */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-    /* USER CODE BEGIN 6 */
+  /* USER CODE BEGIN 6 */
     /* User can add his own implementation to report the file name and line number,
        ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-    /* USER CODE END 6 */
+  /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
