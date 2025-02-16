f228b099e462ddd1ae1655093ad48416b200464b
JiuGe101 <780338669@qq.com>
2025-02-16 16:17:15 +0800

    driver.h

---
+++ 04_Software/project/BSP/WT588/Inc/bsp_wt588_driver.h
@@ -0,0 +1,42 @@
+#ifndef __BSP_WT588_DRIVER_H__
+#define __BSP_WT588_DRIVER_H__
+
+#include <stdint.h>
+#include <stdbool.h>
+typedef struct
+{
+    void (*pf_init)(void);
+    void (*pf_io_write)(uint8_t level);
+}spk_gpio_interface_t;
+
+typedef struct
+{
+    void (*pf_init)(void);
+    void (*pf_is_speaker_busy)(void);
+}spk_busy_interface_t;
+
+typedef struct
+{
+    void (*pf_delay_us)(uint32_t us);
+    void (*pf_delay_ms)(uint32_t ms);
+}sys_interface_t;
+
+typedef struct
+{
+    spk_gpio_interface_t *p_spk_gpiio_interface;
+    spk_busy_interface_t *p_spk_busy_interface;
+    sys_interface_t      *p_sys_interface;
+    int8_t (*pf_start_play)     (void *wt588_driver, uint8_t index);
+    int8_t (*pf_stop_play)      (void *wt588_driver);
+    int8_t (*pf_set_volume)     (void *wt588_driver, uint8_t volume);
+    bool   (*pf_is_speaker_busy)(void *wt588_driver);
+}wt588_driver_t;
+
+int8_t wt588_driver_inst(
+                         wt588_driver_t       *p_wt588_driver,
+                         spk_gpio_interface_t *p_spk_gpiio_interface,
+                         spk_busy_interface_t *p_spk_busy_interface,
+                         sys_interface_t      *p_sys_interface
+                        );
+
+#endif /* __BSP_WT588_DRIVER_H__ */
