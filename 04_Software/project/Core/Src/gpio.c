beb1c72d02acd53e8a35de9351ce3d20c132b7ea
JiuGe101 <780338669@qq.com>
2025-02-16 22:36:13 +0800

    optimize

--- 04_Software/project/Core/Src/gpio.c
+++ 04_Software/project/Core/Src/gpio.c
@@ -42,13 +42,43 @@
 void MX_GPIO_Init(void)
 {
 
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PA0 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PA1 */
+  GPIO_InitStruct.Pin = GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI0_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI0_IRQn);
+
 }
 
 /* USER CODE BEGIN 2 */
-
+#define PA1_GPIO_Port GPIOA
+#define PA1_Pin       GPIO_PIN_1
+__attribute__((used, section("mycallback")))
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) 
+{
+	HAL_GPIO_WritePin(PA1_GPIO_Port, PA1_Pin, 1);
+	HAL_GPIO_WritePin(PA1_GPIO_Port, PA1_Pin, 0);
+	
+}
 /* USER CODE END 2 */
